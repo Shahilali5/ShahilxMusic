6f77d64172063200eec27fd4b91fcce2b5854aec
SHAHIL <110395532+Shahilali5@users.noreply.github.com>
2025-02-06 19:30:36 +0530

    Update authtoken.py

--- ChampuMusic/plugins/tools/authtoken.py
+++ ChampuMusic/plugins/tools/authtoken.py
@@ -1,99 +1,136 @@
 import asyncio
+import glob
+import os
+import random
+from typing import Union
 
-from pyrogram.enums import ChatType
+from pyrogram import filters
+from yt_dlp import YoutubeDL
 
-import config
 from ChampuMusic import app
-from ChampuMusic.core.call import Champu
-from ChampuMusic.core.call import _st_ as clean
-from ChampuMusic.utils.database import (
-    get_active_chats,
-    get_assistant,
-    get_client,
-    is_active_chat,
-    is_autoend,
+from ChampuMusic.misc import SUDOERS
+
+
+def get_random_cookie():
+    folder_path = f"{os.getcwd()}/cookies"
+    txt_files = glob.glob(os.path.join(folder_path, "*.txt"))
+    if not txt_files:
+        raise FileNotFoundError("No .txt files found in the specified folder.")
+    return random.choice(txt_files)
+
+
+class YouTubeAuthDownloader:
+    def __init__(self):
+        self.base_url = "https://www.youtube.com/watch?v="
+
+    def get_ytdl_options(self, ytdl_opts, auth_token: str) -> Union[str, dict, list]:
+        if isinstance(ytdl_opts, list):
+            ytdl_opts += ["--username", "oauth2", "--password", auth_token]
+        elif isinstance(ytdl_opts, str):
+            ytdl_opts += f"--username oauth2 --password {auth_token} "
+        elif isinstance(ytdl_opts, dict):
+            ytdl_opts.update({"username": "oauth2", "password": auth_token})
+        return ytdl_opts
+
+    async def download(self, link: str, auth_token: str, video: bool = True) -> str:
+        loop = asyncio.get_running_loop()
+
+        def download_content():
+            ydl_opts = {
+                "format": (
+                    "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])"
+                    if video
+                    else "bestaudio/best"
+                ),
+                "outtmpl": "downloads/%(id)s.%(ext)s",
+                "geo_bypass": True,
+                "nocheckcertificate": True,
+                "quiet": True,
+                "no_warnings": True,
+            }
+            ydl_opts = self.get_ytdl_options(ydl_opts, auth_token)
+
+            ydl = YoutubeDL(ydl_opts)
+            info = ydl.extract_info(link, download=False)
+            file_path = os.path.join("downloads", f"{info['id']}.{info['ext']}")
+            if not os.path.exists(file_path):
+                ydl.download([link])
+            return file_path
+
+        file_path = await loop.run_in_executor(None, download_content)
+        return file_path
+
+
+async def check_cookies(video_url):
+    cookie_file = get_random_cookie()
+    opts = {
+        "format": "bestaudio",
+        "quiet": True,
+        "cookiefile": cookie_file,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl.extract_info(video_url, download=False)
+        return True
+    except:
+        return False
+
+
+async def check_auth_token():
+    video_url = "https://www.youtube.com/watch?v=LLF3GMfNEYU"
+    auth_token = os.getenv("TOKEN_DATA")
+    opts = {
+        "format": "bestaudio",
+        "quiet": True,
+        "username": "oauth2",
+        "password": auth_token,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl.extract_info(video_url, download=False)
+        return True
+    except:
+        return False
+
+
+@app.on_message(
+    filters.command(
+        [
+            "authstatus",
+            "authtoken",
+            "cookies",
+            "cookie",
+            "cookiesstatus",
+            "cookiescheck",
+        ]
+    )
+    & SUDOERS
 )
+async def list_formats(client, message):
+    status_message = "**Status:**\n\n"
+    status_message += "Cookies: Checking...\nAuth Token: Checking..."
+    status_msg = await message.reply_text(status_message)
 
+    cookie_status = await check_cookies("https://www.youtube.com/watch?v=LLF3GMfNEYU")
+    status_message = "**Status:**\n\n"
+    status_message += f"Cookies: {'✅ Alive' if cookie_status else '❌ Dead'}\nAuth Token: Checking..."
+    await status_msg.edit_text(status_message)
 
-async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT == str(True):
-        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
-            from ChampuMusic.core.userbot import assistants
+    use_token = await check_auth_token()
+    status_message = "**Status:**\n\n"
+    status_message += f"Cookies: {'✅ Alive' if cookie_status else '❌ Dead'}\n"
+    status_message += f"Auth Token: {'✅ Alive' if use_token else '❌ Dead'}"
+    await status_msg.edit_text(status_message)
 
-            for num in assistants:
-                client = await get_client(num)
-                left = 0
-                try:
-                    async for i in client.get_dialogs():
-                        chat_type = i.chat.type
-                        if chat_type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
-                        ]:
-                            chat_id = i.chat.id
-                            if chat_id not in [
-                                config.LOGGER_ID,
-                                -1001961655253,
-                                -1001423108989,
-                            ]:
-                                if left == 20:
-                                    continue
-                                if not await is_active_chat(chat_id):
-                                    try:
-                                        await client.leave_chat(chat_id)
-                                        left += 1
-                                    except:
-                                        continue
-                except:
-                    pass
-
-
-asyncio.create_task(auto_leave())
-
-
-async def auto_end():
-    while not await asyncio.sleep(30):
-        if not await is_autoend():
-            continue
-
-        served_chats = await get_active_chats()
-
-        for chat_id in served_chats:
-            try:
-                if not await is_active_chat(chat_id):
-                    await clean(chat_id)
-                    continue
-
-                userbot = await get_assistant(chat_id)
-                call_participants_id = [
-                    member.chat.id async for member in userbot.get_call_members(chat_id)
-                ]
-
-                if len(call_participants_id) <= 1:
-                    ok = await app.send_message(
-                        chat_id,
-                        "» Nᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ ᴛᴏ sᴏɴɢ ɪɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ.\n"
-                        "ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏᴛʜᴇʀᴡɪsᴇ ʙᴏᴛ ᴡɪʟʟ ᴇɴᴅ sᴏɴɢ ɪɴ 15 sᴇᴄᴏɴᴅs.",
-                    )
-                    await asyncio.sleep(15)
-
-                    call_participants_id = [
-                        member.chat.id
-                        async for member in userbot.get_call_members(chat_id)
-                    ]
-
-                    if len(call_participants_id) <= 1:
-                        await ok.delete()
-                        await Champu.stop_stream(chat_id)
-                        await app.send_message(
-                            chat_id,
-                            "» Nᴏ ᴏɴᴇ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ, sᴏ ᴛʜᴇ sᴏɴɢ ɪs ᴇɴᴅɪɴɢ ᴅᴜᴇ ᴛᴏ ɪɴᴀᴄᴛɪᴠɪᴛʏ.",
-                        )
-                        await clean(chat_id)
-            except:
-                continue
-
-
-# Start the auto_end task
-asyncio.create_task(auto_end())
+    if not use_token:
+        status_message += "\n\n**Generating a new Auth token...**"
+        await status_msg.edit_text(status_message)
+        try:
+            os.system(
+                f"yt-dlp --username oauth2 --password '' -F https://www.youtube.com/watch?v=LLF3GMfNEYU"
+            )
+            await message.reply_text(f"\n**✅ Successfully generated a new token.**")
+        except Exception as ex:
+            await message.reply_text(
+                f"\n**❌ Failed to generate a new token: {str(ex)}**"
+            )
